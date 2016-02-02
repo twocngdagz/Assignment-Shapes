48a804388c89f4459cd33e8ebb9c36c3a9b5181d
Marco Lumia <lumiamarco@gmail.com>
2016-02-01 20:58:13 -0800

    organize

--- shapes.php
+++ shapes.php
@@ -3,19 +3,25 @@
 include_once('config/global.php');
 include_once('libs/shape-calculator/src/ShapeCalculator/Calculator.php');
 include_once('libs/shape-calculator/src/ShapeCalculator/Square.php');
+include_once('libs/shape-calculator/src/ShapeCalculator/Circle.php');
 include_once('libs/shape-calculator/src/ShapeCalculator/Cube.php');
+include_once('libs/shape-calculator/src/ShapeCalculator/Sphere.php');
+
+// The calculator
+$calculator = new \Libraries\ShapeCalculator\Calculator;
 
 // Define our shapes
 $square = new \Libraries\ShapeCalculator\Square(25);
+$circle = new \Libraries\ShapeCalculator\Circle(90);
 $cube = new \Libraries\ShapeCalculator\Cube(33);
+$sphere = new \Libraries\ShapeCalculator\Sphere(18);
 
-$surface = $_GET['surface'];
-$shapes = [ $square, $cube ];
-$calculator = new \Libraries\ShapeCalculator\Calculator;
-
-// Get the surface area covered by the shapes
+$shapes = [ $square, $circle, $cube, $sphere ];
 $area = $calculator->surfaceArea($shapes);
 $volume = $calculator->totalVolume($shapes);
 
-echo "The surface area covered is $area\n";
+// Output the total surface area of the shapes
+echo "The total surface area is $area\n";
+
+// Output the total volume of the shapes
 echo "The total volume is $volume\n";
